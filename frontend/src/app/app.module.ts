463fb4d8cc5f6ee611517b56becfb35b164685c5
elaa <elaa.brahmii@gmail.com>
2025-04-29 21:52:29 +0100

    created 3 components in page folder

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { LinksComponent } from './pages/links/links.component';
+import { CustomLinksComponent } from './pages/custom-links/custom-links.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    LinksComponent,
+    CustomLinksComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/pages/custom-links/custom-links.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-links',
+  templateUrl: './custom-links.component.html',
+  styleUrls: ['./custom-links.component.css']
+})
+export class CustomLinksComponent {
+
+}
