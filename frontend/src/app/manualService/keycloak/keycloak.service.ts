8eb7f5452584b8b56edc3d87e6af6fae775c2490
elaa <elaa.brahmii@gmail.com>
2025-05-11 21:32:26 +0100

    getting done with keycloak setup

--- frontend/src/app/manualService/keycloak/keycloak.service.ts
+++ frontend/src/app/manualService/keycloak/keycloak.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
-
+import Keycloak from 'keycloak-js';
+import { UserProfile } from './user-profile';
+//will be started when the app is started
 @Injectable({
   providedIn: 'root'
 })
 export class KeycloakService {
+  private _keycloak: Keycloak | undefined;
+  private profile:UserProfile|undefined;
+  get userProfile(): UserProfile | undefined {
+    return this.profile;
+  }
 
+  get keycloak() {
+    if(!this._keycloak) {
+      this._keycloak = new Keycloak({
+        url: 'http://localhost:9090',
+        realm: 'url-shortening',
+        clientId: 'url'
+
+    });}
+    return this._keycloak;
+  }
   constructor() { }
+  async init() { //runs when the app is started
+    console.log('Authenticating the user ...');
+    const authenticated = await this.keycloak?.init({
+      onLoad: 'login-required',
+
+  });
+    if (authenticated) {
+      console.log('User authenticated');
+      this.profile = (await this.keycloak?.loadUserProfile()) as UserProfile;
+      this.profile.token = this.keycloak?.token;
+      console.log('User profile loaded', this.profile);
+    }
+  }
+  login() {
+    this.keycloak?.login();
+  }
+  logout() {
+    this.keycloak?.logout({redirectUri: 'http://localhost:4200'});
+  }
+
+
+
 }
