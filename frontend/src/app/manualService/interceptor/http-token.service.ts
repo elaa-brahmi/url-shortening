216f4d4679e54c4537bcba6cdb70da0fdc871dcd
elaa <elaa.brahmii@gmail.com>
2025-05-12 09:33:40 +0100

    injecting token in all http requests

--- frontend/src/app/manualService/interceptor/http-token.service.ts
+++ frontend/src/app/manualService/interceptor/http-token.service.ts
@@ -6,10 +6,12 @@ import { KeycloakService } from '../keycloak/keycloak.service';
 @Injectable({
   providedIn: 'root'
 })
+//attach an authorization token (retrieved from the KeycloakService) to the request headers if the token is available
 export class HttpTokenService implements HttpInterceptor {
 
     constructor(private keycloakService: KeycloakService) {
     }
+    //This service is typically used to ensure that all outgoing HTTP requests include the user's authentication token, enabling secure communication with backend APIs that require authentication.
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const token = this.keycloakService.keycloak.token;
     if (token) {
